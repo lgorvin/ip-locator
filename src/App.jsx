c8abadfca29ba4b3678e28a6a828e47bc49eeb00
lgorvin <lgorvin78@gmail.com>
2022-10-19 16:43:04 +0100

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 import ReactLoading from "react-loading";
 
-import { apiKey } from "./newapi";
+import { apiKey } from "./api";
 
 const App = () => {
   const [address, setAddress] = useState("");
