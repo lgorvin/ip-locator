e581fc1c003ae0bb161c4a83daa7c9a4a1a69f25
lgorvin <lgorvin78@gmail.com>
2022-10-19 15:50:25 +0100

    fully functioning ip tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,12 @@
 import axios from "axios";
-import React, { useRef } from "react";
+import React from "react";
 import { useState, useEffect } from "react";
-import {
-  MapContainer,
-  TileLayer,
-  useMap,
-  Marker,
-  Popup,
-  setView,
-  useMapEvent,
-} from "react-leaflet";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 import ReactLoading from "react-loading";
 
+import { apiKey } from "./api";
+
 const App = () => {
-  // const url =
-  //   "https://geo.ipify.org/api/v2/country,city?apiKey=at_gwdGvwCRWQMN5AeWtZDcJCraMgAwc&ipAddress=8.8.8.8";
-
-  //const url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_gwdGvwCRWQMN5AeWtZDcJCraMgAwc&ipAddress=${test}`;
-
-  // try {
-  //   setLoading(true);
-  //   axios
-  //     .get(
-  //       `"https://api.ipdata.co/${test}?api-key=98e10052fe66cd7d5f7ab713e9f8f83918cab5aff563099731cb6884"`
-  //     )
-  //     .then((res) => {
-  //       setAddress(res.data.ip);
-  //       setLocation(`${res.data.location.city}, ${res.data.location.region}`);
-  //       setTimezone(res.data.location.timezone);
-  //       setIsp(res.data.isp);
-  //       setLat(res.data.location.lat);
-  //       setLong(res.data.location.lng);
-  //       setLoading(false);
-  //     });
-  // } catch (error) {
-  //   setLoading(false);
-  //   console.log(error.response);
-  // }
-
   const [address, setAddress] = useState("");
   const [location, setLocation] = useState("");
   const [timezone, setTimezone] = useState("");
@@ -46,11 +15,7 @@ const App = () => {
   const [isp, setIsp] = useState("");
   const [test, setTest] = useState("");
 
-  const [position, setPosition] = useState(null);
-
   const [loading, setLoading] = useState(false);
-
-  const mapRef = useRef();
 
   function MyComponent() {
     // const map = useMapEvent("click", () => {
@@ -62,18 +27,14 @@ const App = () => {
 
     useEffect(() => {
       map.flyTo([lat, long]);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
   }
-
-  //const key = "98e10052fe66cd7d5f7ab713e9f8f83918cab5aff563099731cb6884";
-
   const fetchAdvice = async () => {
     try {
       setLoading(true);
       axios
-        .get(
-          `https://api.ipdata.co/${test}?api-key=98e10052fe66cd7d5f7ab713e9f8f83918cab5aff563099731cb6884`
-        )
+        .get(`https://api.ipdata.co/${test}?api-key=${apiKey}`)
         .then((res) => {
           setAddress(res.data.ip);
           setLocation(`${res.data.city}, ${res.data.region}`);
@@ -89,29 +50,34 @@ const App = () => {
     }
   };
 
-  console.log(lat);
-  console.log(long);
+  const onKeyUp = (event) => {
+    if (event.charCode === 13) {
+      fetchAdvice();
+    }
+  };
 
   useEffect(() => {
     fetchAdvice();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div className="bg1 mt-[-25px] h-[320px] pb-[30px]">
-      <h1 className="text-center pt-12 font-medium text-white text-2xl">
+      <h1 className="text-center pt-12 lg:pt-16 font-medium text-white text-2xl lg:text-4xl duration-500">
         IP Address Tracker
       </h1>
-      <div className="mt-6 flex justify-center">
+      <div className="mt-6 lg:mt-12 flex justify-center">
         <input
-          className="h-14 w-[264px] ml-6 rounded-l-lg px-4 shadow-lg"
+          className="h-14 w-[264px] lg:w-[550px] ml-6 rounded-l-lg px-4 shadow-lg duration-700"
           placeholder="Enter an IP Address"
           value={test}
           onChange={(e) => setTest(e.target.value)}
+          onKeyPress={onKeyUp}
           type="text"
         />
         <div
           onClick={fetchAdvice}
-          className="rounded-r-lg mr-6 bg-black shadow-lg h-14 w-14 cursor-pointer"
+          className="rounded-r-lg mr-6 bg-black  hover:bg-gray-800 duration-300 shadow-lg h-14 w-14 cursor-pointer"
         >
           {loading ? (
             <div className="float-right mt-4 mr-4">
@@ -124,7 +90,7 @@ const App = () => {
             </div>
           ) : (
             <svg
-              className="float-right mt-5 mr-6"
+              className="float-right mt-5 mr-6 hover:scale-125 duration-300"
               xmlns="http://www.w3.org/2000/svg"
               width="11"
               height="14"
@@ -140,78 +106,86 @@ const App = () => {
         </div>
       </div>
       <div className="flex justify-center">
-        <div className="bg-white h-[266px] opacity-95 shadow-lg w-80 mx-6 mt-6 rounded-lg">
-          <h1 className="text-center text-xs text-gray-400 font-bold pt-5">
-            IP ADDRESS
-          </h1>
-          <h2 className="text-center text-xl pt-1 font-medium">
-            {loading ? (
-              <div className="flex justify-center">
-                <ReactLoading
-                  type={"bubbles"}
-                  color={"#0DC6DE"}
-                  height={25}
-                  width={25}
-                />
-              </div>
-            ) : (
-              address
-            )}
-          </h2>
-          <h1 className="text-center text-xs text-gray-400 font-bold pt-3">
-            LOCATION
-          </h1>
-          <h2 className="text-center text-xl pt-1 font-medium">
-            {loading ? (
-              <div className="flex justify-center">
-                <ReactLoading
-                  type={"bubbles"}
-                  color={"#0DC6DE"}
-                  height={25}
-                  width={25}
-                />
-              </div>
-            ) : (
-              location
-            )}
-          </h2>
-          <h1 className="text-center text-xs text-gray-400 font-bold pt-3">
-            TIMEZONE
-          </h1>
-          <h2 className="text-center text-xl pt-1 font-medium">
-            {loading ? (
-              <div className="flex justify-center">
-                <ReactLoading
-                  type={"bubbles"}
-                  color={"#0DC6DE"}
-                  height={25}
-                  width={25}
-                />
-              </div>
-            ) : (
-              <>UTC {timezone}</>
-            )}
-          </h2>
-          <h1 className="text-center text-xs text-gray-400 font-bold pt-3">
-            ISP
-          </h1>
-          <h2 className="text-center text-xl pt-1 font-medium">
-            {loading ? (
-              <div className="flex justify-center">
-                <ReactLoading
-                  type={"bubbles"}
-                  color={"#0DC6DE"}
-                  height={25}
-                  width={25}
-                />
-              </div>
-            ) : (
-              isp
-            )}
-          </h2>
+        <div className="bg-white h-auto min-w-[326px] lg:min-w-[900px] opacity-95 shadow-lg mx-6 mt-6 lg:mt-10 rounded-lg grid grid-cols-1 lg:grid-cols-4 duration-700">
+          <div className=" lg:mt-10 lg:mb-10">
+            <h1 className="text-center text-xs text-gray-400 font-bold pt-3">
+              IP ADDRESS
+            </h1>
+            <h2 className="text-center text-xl mx-6 pt-1 font-medium">
+              {loading ? (
+                <div className="flex justify-center">
+                  <ReactLoading
+                    type={"bubbles"}
+                    color={"#0DC6DE"}
+                    height={25}
+                    width={25}
+                  />
+                </div>
+              ) : (
+                address
+              )}
+            </h2>
+          </div>
+          <div className="lg:mt-10 lg:mb-10">
+            <h1 className="text-center text-xs text-gray-400 font-bold pt-3">
+              LOCATION
+            </h1>
+            <h2 className="text-center text-xl pt-1 mx-6 font-medium">
+              {loading ? (
+                <div className="flex justify-center">
+                  <ReactLoading
+                    type={"bubbles"}
+                    color={"#0DC6DE"}
+                    height={25}
+                    width={25}
+                  />
+                </div>
+              ) : (
+                location
+              )}
+            </h2>
+          </div>
+          <div className="lg:mt-10 lg:mb-10">
+            <h1 className="text-center text-xs text-gray-400 font-bold pt-3">
+              TIMEZONE
+            </h1>
+            <h2 className="text-center text-xl pt-1 mx-6 font-medium">
+              {loading ? (
+                <div className="flex justify-center">
+                  <ReactLoading
+                    type={"bubbles"}
+                    color={"#0DC6DE"}
+                    height={25}
+                    width={25}
+                  />
+                </div>
+              ) : (
+                <>UTC {timezone}</>
+              )}
+            </h2>
+          </div>
+          <div className="mb-2 lg:mt-10 lg:mb-10">
+            <h1 className="text-center text-xs text-gray-400 font-bold pt-3">
+              ISP
+            </h1>
+            <h2 className="text-center text-xl pt-1 mx-6 font-medium">
+              {loading ? (
+                <div className="flex justify-center">
+                  <ReactLoading
+                    type={"bubbles"}
+                    color={"#0DC6DE"}
+                    height={25}
+                    width={25}
+                  />
+                </div>
+              ) : (
+                isp
+              )}
+            </h2>
+          </div>
         </div>
       </div>
-      <div className="absolute behind mt-[80px]">
+      <div className="absolute behind mt-[180px] lg:mt-[250px] duration-500">
         <MapContainer center={[lat, long]} zoom={14} scrollWheelZoom={true}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
